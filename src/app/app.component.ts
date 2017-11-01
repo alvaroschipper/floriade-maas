5b0ca1b84de12249c314d71da3e4d723d502f0a1
RemarkableGuy <alvaro.schipper@hotmail.com>
2017-11-01 16:12:29 +0100

    Added most basic AGM implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Floriade MaaS';
+  lat: number = 52.375241;
+  lng: number = 5.219354;
 }
