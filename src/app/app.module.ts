b2f71a3dd5b9b060aa0bdc013c861fa210df2235
Shakie123 <sharieforie05@live.nl>
2017-11-07 13:41:13 +0100

    Added key reminder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { GeoLocationService } from './services/geo-location.service';
   imports: [
     BrowserModule,
     AgmCoreModule.forRoot({
+      //Don't forget to remove the key
       apiKey: ''
     })
   ],
