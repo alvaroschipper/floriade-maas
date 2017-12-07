08153aa0b6280d733e8f7033d0267bfdc484c0a4
Shakie123 <sharieforie05@live.nl>
2017-12-07 12:27:50 +0100

    Fixed color place-options types Fixed responsibility for mobile and larger screens Fixed navbar to top of screen Bumped version

--- src/app/place-options/place-options.component.ts
+++ src/app/place-options/place-options.component.ts
@@ -21,11 +21,11 @@ export class PlaceOptionsComponent implements OnInit, AfterViewInit {
         category: 'Food',
         categoryIcon: 'restaurant',
         types: [
-          {name: 'Bakery', type: 'bakery', color: 'red darken-2'},
-          {name: 'Cafe', type: 'cafe', color: 'indigo darken-2'},
-          {name: 'Meal Takeaway', type: 'meal_takeaway', color: 'deep-orange darken-2'},
-          {name: 'Restaurant', type: 'restaurant', color: 'light-green darken-2ull'},
-          {name: 'Liquor Store', type: 'liquor_store', color: 'teal darken-2'}
+          {name: 'Bakery', type: 'bakery', color: 'red darken-1'},
+          {name: 'Cafe', type: 'cafe', color: 'orange darken-1'},
+          {name: 'Meal Takeaway', type: 'meal_takeaway', color: 'pink darken-1'},
+          {name: 'Restaurant', type: 'restaurant', color: 'blue darken-1'},
+          {name: 'Liquor Store', type: 'liquor_store', color: 'purple darken-1'}
         ]
       },
 
@@ -33,49 +33,49 @@ export class PlaceOptionsComponent implements OnInit, AfterViewInit {
         category: 'Entertainment',
         categoryIcon: 'theaters',
         types: [
-          {name: 'Amusement Park', type: 'amusement_park', color: 'red darken-2'},
-          {name: 'Library', type: 'library', color: 'indigo darken-2'},
-          {name: 'Casino', type: 'casino', color: 'deep-orange darken-2'},
-          {name: 'Cinema', type: 'movie_theater', color: 'light-green darken-2ull'},
-          {name: 'Park', type: 'park', color: 'teal darken-2'},
+          {name: 'Amusement Park', type: 'amusement_park', color: 'red darken-1'},
+          {name: 'Library', type: 'library', color: 'orange darken-1'},
+          {name: 'Casino', type: 'casino', color: 'pink darken-1'},
+          {name: 'Cinema', type: 'movie_theater', color: 'blue darken-1'},
+          {name: 'Park', type: 'park', color: 'purple darken-1'},
         ]
       },
       {
         category: 'Shopping',
         categoryIcon: 'shopping_cart',
         types: [
-          {name: 'Book Store', type: 'book_store', color: 'red darken-2'},
-          {name: 'Clothing Store', type: 'clothing_store', color: 'indigo darken-2'},
-          {name: 'Convenience Store', type: 'convenienc  e_store', color: 'deep-orange darken-2'},
-          {name: 'Department Store', type: 'department_store', color: 'light-green darken-2ull'},
-          {name: 'Electronics Store', type: 'electronics_store', color: 'teal darken-2'},
-          {name: 'Shoe Store', type: 'shoe_store', color:  'lime darken-2'},
+          {name: 'Book Store', type: 'book_store', color: 'red darken-1'},
+          {name: 'Clothing Store', type: 'clothing_store', color: 'orange darken-1'},
+          {name: 'Convenience Store', type: 'convenienc  e_store', color: 'pink darken-1'},
+          {name: 'Department Store', type: 'department_store', color: 'blue darken-1'},
+          {name: 'Electronics Store', type: 'electronics_store', color: 'purple darken-1'},
+          {name: 'Shoe Store', type: 'shoe_store', color:  'teal darken-1'},
         ]
       },
       {
         category: 'Beauty',
         categoryIcon: 'spa',
         types: [
-          {name: 'Beauty Salon', type: 'beauty_salon', color: 'red darken-2'},
-          {name: 'Hair Care', type: 'hair_care', color: 'indigo darken-2'},
-          {name: 'Spa', type: 'spa', color: 'deep-orange darken-2'},
+          {name: 'Beauty Salon', type: 'beauty_salon', color: 'red darken-1'},
+          {name: 'Hair Care', type: 'hair_care', color: 'orange darken-1'},
+          {name: 'Spa', type: 'spa', color: 'pink darken-1'},
         ]
       },
       {
         category: 'Health',
         categoryIcon: 'local_hospital',
         types: [
-          {name: 'Hospital', type: 'hospital', color: 'red darken-2'},
-          {name: 'Doctor', type: 'doctor', color: 'indigo darken-2'},
-          {name: 'Pharmacy', type: 'pharmacy', color: 'deep-orange darken-2'}
+          {name: 'Hospital', type: 'hospital', color: 'red darken-1'},
+          {name: 'Doctor', type: 'doctor', color: 'orange darken-1'},
+          {name: 'Pharmacy', type: 'pharmacy', color: 'pink darken-1'}
         ]
       },
       {
         category: 'Cash',
         categoryIcon: 'local_atm',
         types: [
-          {name: 'ATM', type: 'atm', color: 'red darken-2'},
-          {name: 'Bank', type: 'bank', color: 'indigo darken-2'}
+          {name: 'ATM', type: 'atm', color: 'red darken-1'},
+          {name: 'Bank', type: 'bank', color: 'orange darken-1'}
         ]
       },
     ];
